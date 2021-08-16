fcae3b7f38990fb199983d4ecb19a4eb773d860a
syougo <supernaturalsyougo@gmail.com>
2021-08-16 20:53:35 +0900

    復習完了

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,21 @@
 class UsersController < ApplicationController
   skip_before_action :login_required, only:[ :new, :create ]
   def show
+    @user = User.find(params[:id])
   end
 
   def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:notice] = '更新しました'
+      redirect_to blogs_path
+    else
+      render :edit
+    end
   end
 
   def new
